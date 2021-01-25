415b2273fd10d8e3f1960d1e33bf393b13c0be42
Hillary Gil <hillarygil97@gmail.com>
2021-01-24 20:39:57 -0600

    extracción de infomrmación de una API usando FETCH

--- js/index.js
+++ js/index.js
@@ -1,38 +1,48 @@
-const printData = response =>{
+const printData = data =>{
     const wrapper = document.createElement('article')
-    const name = document.createElement('h1')
-    const image = document.createElement('img')
+    const model = document.createElement('h1')
+    const image = document.createElement('div')
+    const make = document.createElement('h2')
     const id = document.createElement('p')
+    const km =document.createElement('p')
+    const description = document.createElement('p')
+    const calendar = document.createElement('div')
+    const estimateDate = document.createElement('p')
+    const estimateDelivery = document.createElement('p')
 
-    name.innerHTML = response.name
-    id.innerHTML = response.id
-    image.setAttribute('src',response.image)
-    wrapper.appendChild(name)
+
+    model.innerHTML = `Modelo: ${data.model}`
+    make.innerHTML = `Marca: ${data.make}`
+    id.innerHTML = `No. de ID: ${data.id}`
+    km.innerHTML = `Kilometraje: ${data.km}`
+    description.innerHTML = `Descripción: ${data.description}`
+    estimateDate.innerHTML = `Fecha de llegada: ${data.estimateDate}`
+    estimateDelivery.innerHTML = `Fecha de entrega: ${data.estimateDeliveryDate}`
+
+    calendar.setAttribute("class",'calendar')
+    image.setAttribute("class",'image')
+    image.setAttribute("style", `background-image: url('${data.image}')`)
+    
+    wrapper.appendChild(model)
     wrapper.appendChild(image)
+    wrapper.appendChild(make)
     wrapper.appendChild(id)
+    wrapper.appendChild(km)
+    wrapper.appendChild(description)
+    wrapper.appendChild(calendar)
+    calendar.appendChild(estimateDate)
+    calendar.appendChild(estimateDelivery)
     const container = document.getElementById('container')
     container.appendChild(wrapper)
 }
 
-function loadDoc () {
-    //1
-    var xhttp = new XMLHttpRequest()
-    xhttp.onreadystatechange = function(){
-        //2
-        if(this.readyState === 4 && this.status === 200){
-            const response = JSON.parse(this.responseText).results
-            console.log(response)
-            //Las funciones que queremos que se ejecute
-            //ToDo: printData()
-            response.forEach(element => {
-                printData(element)
-            })
-        }
-    }
-    //3
-    xhttp.open('GET', 'https://rickandmortyapi.com/api/character')
-    //4
-    xhttp.send()
-}
 
-loadDoc()
+
+fetch('https://ecb-server.arianaomi.vercel.app/cars')
+  .then(response => response.json())
+  .then((data) => {
+      //console.log(data.dataCars)
+      data.dataCars.forEach(element => {
+        printData(element)
+    })
+});
